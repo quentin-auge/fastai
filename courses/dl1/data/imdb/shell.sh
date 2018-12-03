9947d3ad48f0abac95300dbc35e2e89d4f798c2f
Quentin Augé <quentin.auge@gmail.com>
2018-12-03 21:47:42 +0100

    add new gender data

--- courses/dl1/data/imdb/shell.sh
+++ courses/dl1/data/imdb/shell.sh
@@ -6,26 +6,26 @@ gcloud compute ssh jupyter@fastai-vm
 cd ~/tutorials/fastai/courses/dl1/
 mkdir -p data/
 cd data/
-wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_0.tar
-tar xvf imdb_0.tar
 wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_meta.tar
 tar xvf imdb_meta.tar
+wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_0.tar
+wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_2.tar
+tar xvf imdb_0.tar
+tar xvf imdb_1.tar
 cd imdb/
-mv 0{0,1,2,3,4,5,6,7}/ train/
-mv 0{8,9}/ test/
-mkdir -p sample/train/00/
-cp train/00/nm0000100_* sample/train/00/
-mkdir -p sample/train/07/
-cp train/07/nm0000107_* sample/train/07/
-mkdir -p sample/test/09/
-cp test/09/nm0000009_* sample/test/09/
+mv 0{0,1,2,3,4,5,6,7,8,9}/ train/
+mv 1{0,1,2,3,4,5,6,7,8,9}/ train/
+mkdir -p sample/00/
+cp train/00/nm0000100_* sample/00/
+mkdir -p sample/07/
+cp train/07/nm0000107_* sample/07/
 
 
 # Resizing
 
 # Local
 
-cd ~/dl/fastai/courses/dl1/data/imdb/sample/train/
+cd ~/dl/fastai/courses/dl1/data/imdb/sample/
 
 # Look at images size
 for f in ./*/*.jpg; do
@@ -44,7 +44,8 @@ gcloud compute ssh jupyter@fastai-vm
 
 cd ~/tutorials/fastai/courses/dl1/data/imdb/train/
 
-mkdir -p resized/0{0,1,2,3,4,5,6,7}/
+mkdir -p resized/0{0,1,2,3,4,5,6,7,8,9}/
+mkdir -p resized/1{0,1,2,3,4,5,6,7,8,9}/
 for f in ./*/*.jpg; do
   echo "$f"
   convert "$f" -resize 224x224^ "./resized/$f"
