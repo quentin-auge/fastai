b4b1fd4e0e45ff4bf1674a9be85e936fbf9af399
Quentin Augé <quentin.auge@gmail.com>
2018-12-16 18:28:36 +0000

    make lang_model-arxiv.ipynb run .....

--- courses/dl1/data/arxiv/shell.sh
+++ courses/dl1/data/arxiv/shell.sh
@@ -3,4 +3,7 @@
 # Setup
 pip install feedparser
 python -m spacy download en
+
+# https://forums.fast.ai/t/error-while-running-lesson-4-notebook/23872/3
 pip install torch==0.4.1
+conda install pytorch
